3ec75c04e9560c8068d38b0b0b3292e8a527f2a8
tanyo <tanyojo@gmail.com>
2024-02-06 02:07:53 -0500

    worked on Exams.java

--- app/src/main/java/com/example/collegescheduler2/ui/exams/ExamsFragment.java
+++ app/src/main/java/com/example/collegescheduler2/ui/exams/ExamsFragment.java
@@ -4,39 +4,50 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.collegescheduler2.Course;
 import com.example.collegescheduler2.R;
 import com.example.collegescheduler2.databinding.FragmentExamsBinding;
+import com.example.collegescheduler2.ui.CustomAdapter;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 
 public class ExamsFragment extends Fragment {
-    private ArrayList<Course> Exams;
+    private ArrayList<Exam> Exams;
 
     private FragmentExamsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ExamsViewModel examsViewModel = new ViewModelProvider(this).get(ExamsViewModel.class);
+
         View view = inflater.inflate(R.layout.fragment_exams, container, false);
+        Exams = new ArrayList<Exam>();
 
-        Exams = new ArrayList<>();
-        Exams.add(new Exam("English", new Date(0, 1, 1), "John"));
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        CustomAdapter<Course> adapter = new CustomAdapter<Course>(courses);
+        recyclerView.setAdapter(adapter);
 
-        binding = FragmentExamsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        ItemTouchHelper helper = new ItemTouchHelper(swipeToDelete(adapter));
+        helper.attachToRecyclerView(recyclerView);
 
-        final TextView textView = binding.textSlideshow;
-        examsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        Button button = view.findViewById(R.id.addCourseButton);
+        button.setOnClickListener(v -> {
+        });
+        return view;
     }
 
     @Override
